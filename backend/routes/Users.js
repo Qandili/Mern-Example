53e8610df6598d57f0b0a963b4aef6f462a6269e
oussidi mohamed <osidi1998@gmail.com>
2019-10-20 19:31:25 +0100

    oussidi changes

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -1,22 +1,24 @@
-const router = require('express').Router();
-let User = require('../models/user.model');
+const router = require("express").Router();
+let User = require("../Models/User");
 //the find method is a mongoDB method that returns a promiss so the result are returned in Json format
 //
-router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.get("/", (req, res) => {
+    res.send({ username: "oussidi" });
+    // User.find()
+    //     .then(users => res.json(users))
+    //     .catch(err => res.status(400).json("Error: " + err));
 });
 
 // endPoint of a post request to register a new user
-router.route('/add').post((req, res) => {
-  const username = req.body.username;
+router.post("/add", (req, res) => {
+    const username = req.body.username;
 
-  const newUser = new User({username});
+    const newUser = new User({ username });
 
-  newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    newUser
+        .save()
+        .then(() => res.json("User added!"))
+        .catch(err => res.status(400).json("Error: " + err));
 });
 // to export the router
 module.exports = router;
